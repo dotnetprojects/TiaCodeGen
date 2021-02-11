79009908ef5602103c79189a441329070860e8f5
jkuehner <jochen.kuehner@kardex.com>
2021-02-11 16:05:48 +0100

    add support for inrange/outrange

---
+++ TiaCodegen/Commands/Comparisons/InRangeCall.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using TiaCodegen.Commands.Functions.Base;
+using TiaCodegen.Enums;
+using TiaCodegen.Interfaces;
+
+namespace TiaCodegen.Commands.Comparisons
+{
+    public class InRangeCall : SystemFunctionCall 
+    {
+        public InRangeCall(
+            IOperationOrSignal min,
+            IOperationOrSignal @in,
+            IOperationOrSignal max,
+            IOperationOrSignal @out,
+            IOperationOrSignal eno = null) : base("InRange", eno)
+        {
+            DisableEno = false;
+            Interface["min"] = new IOperationOrSignalDirectionWrapper(min, Direction.Input);
+            Interface["in"] = new IOperationOrSignalDirectionWrapper(@in, Direction.Input);
+            Interface["max"] = new IOperationOrSignalDirectionWrapper(max, Direction.Output);
+            Interface["out"] = new IOperationOrSignalDirectionWrapper(@out, Direction.Output);
+
+            Children.AddRange(Interface.Values.Where(x => x.OperationOrSignal != null).Select(x => x.OperationOrSignal));
+        }
+    }
+}
---
+++ TiaCodegen/Commands/Functions/Arithmetic/VariableArithmeticCall.cs
@@ -0,0 +1,11 @@
+﻿using TiaCodegen.Interfaces;
+
+namespace TiaCodegen.Commands.Functions.Arithmetic
+{
+    public abstract class VariableArithmeticCall : ArithmeticCall
+    {
+        public VariableArithmeticCall(string functionName, IOperationOrSignal eno = null) : base(functionName, eno)
+        {
+        }
+    }
+}
