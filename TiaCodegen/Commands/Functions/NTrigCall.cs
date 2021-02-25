5aa83f90b844bdea894b9f7550b97ebf556324db
jkuehner <jochen.kuehner@kardex.com>
2021-02-25 23:38:11 +0100

    add ntrig call

---
+++ TiaCodegen/Commands/Functions/NTrigCall.cs
@@ -0,0 +1,16 @@
+﻿using TiaCodegen.Commands.Coils;
+using TiaCodegen.Commands.Signals;
+using TiaCodegen.Interfaces;
+
+namespace TiaCodegen.Commands.Functions
+{
+    public class NTrigCall : BaseNPCoil, IPartName
+    {
+        public NTrigCall(Signal signal) : base(signal, null, null)
+        {
+            PartName = "NBox";
+        }
+
+        public string PartName { get; set; }
+    }
+}
